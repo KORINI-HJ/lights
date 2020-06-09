985456ffe340a20e7cf753d5d86b126e35bd4578
KORINI-HJ <korini@likelion.org>
2020-06-09 21:11:40 +0900

    profile-1

--- petprint/petprint/urls.py
+++ petprint/petprint/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls import url
 from home.views import index, detail, comment_create, DiaryCreateView, DiaryUpdateView, DiaryDeleteView
 from loginapp import views as loginapp_views
 from django.contrib.auth.views import LoginView,LogoutView #sign_up기능은 장고에서 없어서 따로 view에서 함수 써주고 나머지 로그인,로그아웃은 장고에 있어서 따로 그냥 가져옴.
+from userapp.views import profile, profile_edit
 
 
 urlpatterns = [
@@ -35,5 +36,7 @@ urlpatterns = [
     path('loginapp/sign_up/', loginapp_views.sign_up, name="sign_up"),
     path('loginapp/login/',LoginView.as_view(),name="login"),
     path('loginapp/logout/',LogoutView.as_view(),name="logout"),
+    path('userapp/profile/', profile, name="profile"),
+    path('userapp/profile_edit/', profile_edit, name="profile_edit"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
