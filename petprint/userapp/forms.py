73409eddc6221bda7327a508ebeb6a428a9bf9fc
KORINI-HJ <korini@likelion.org>
2020-06-11 00:05:40 +0900

    날씨 이미지 변경 및 프로필 업로드(경로 재설정 필요)

--- petprint/userapp/forms.py
+++ petprint/userapp/forms.py
@@ -1,22 +1,8 @@
 from django import forms
 from .models import Profile
-from django.contrib.auth.models import User
 
 class ProfileForm(forms.ModelForm):
-    
-    def __init__(self, *args, **kwargs):
-        super(ProfileForm, self).__init__(*args, **kwargs)
-        
-        self.fields['name'].widget.attrs.update(
-            {'placeholder': '닉네임',
-            'class': "pf_name fo-rm",
-             'id': "pf_name"})
-
-        self.fields['image'].widget.attrs.update(
-            {'placeholder': '이미지',
-            'class':'fo-rm',
-             'id': "pf_major"})
-    
+    profile_photo = forms.ImageField(required=False) # 선택적으로 입력할 수 있음.
     class Meta:
         model = Profile
-        fields = '__all__'
+        fields = ['nickname','profile_photo']
