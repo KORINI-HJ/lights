0d0085d0dbab206e904a26321dc6b40563e26fdf
KORINI-HJ <korini@likelion.org>
2020-06-10 21:56:20 +0900

    hj

--- petprint/userapp/models.py
+++ petprint/userapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+class Profile(models.Model):
+    name = models.CharField(max_length=20)
+    images = models.ImageField(default='default.jpg', upload_to='%Y/%m/%d/%H/%m')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
--- petprint/userapp/views.py
+++ petprint/userapp/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.mixins import AccessMixin
+from django.urls import reverse_lazy
+from .models import Profile
+from .forms import ProfileForm
 
 # Create your views here.
-def profile(request):
-    return render(request,'profile.html')
-
+'''
 def profile_edit(request):
     return render(request,'profile_edit.html')
+'''
+def profile(request):
+    return render(request, 'profile.html')
+
+class OwnerOnlyMixin(AccessMixin):
+    raise_exception = True
+    permission_denied_message = "Owner only can update/delete the object다."
+
+class ProfileView(OwnerOnlyMixin, CreateView):
+    model = Profile
+    fields = ['name', 'images']
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
